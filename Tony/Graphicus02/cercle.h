c732f526e2545e83443822e8c99413a68feb4bdc
LazerTony20 <antho007@hotmail.ca>
2021-01-18 15:30:30 -0500

    cercle codé et compilé

---
+++ Tony/Graphicus02/cercle.h
@@ -0,0 +1,37 @@
+/********
+ * Fichier: cercle.h
+ * Auteurs: Anthony Royer et Guillaume Beaudoin
+ * Date: 18 janvier 2021 (creation)
+ * Description: Declaration de classe cercle.
+********/
+
+#ifndef CERCLE_H
+#define CERCLE_H
+
+#include <iostream>
+#include "forme.h"
+
+class Cercle:public Forme
+{
+public:
+	//Constructeur
+	Cercle(int reyon = 1); //on met les =1 afin d'avoir des valeurs par défaut pour mon constructeur
+	//Destructeur
+	~Cercle();
+	
+	//Méthodes
+	double aire() = 0;
+	int getRayon();
+	void setDimensions(int newRayon);
+	void afficher(ostream & s);
+private:
+	//Variables
+	int rayon;
+};
+
+
+#endif
+
+
+
+
