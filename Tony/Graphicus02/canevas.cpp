8f3f3690e91efedb3c0496f6071c642304f3b0f1
LazerTony20 <antho007@hotmail.ca>
2021-01-18 23:32:16 -0500

    Final Build Pre-tests

--- Tony/Graphicus02/canevas.cpp
+++ Tony/Graphicus02/canevas.cpp
@@ -61,16 +61,51 @@ bool Canevas::retirerForme(int index)
 
 double Canevas::aire()
 {
-   return 0.0;
+   double aire_totale = 0.0;
+	if(activeLayer == NOACTIVELAYERS){return -1.0;}	//Vibe Check xD.
+	for(int i;i<couches[activeLayer].VecteurFormesCouche.getTaille();i++)
+   {
+      aire_totale += couches[activeLayer].VecteurFormesCouche.getPointeur(i)->aire();
+   }
+   return aire_totale;
 }
 
 bool Canevas::translater(int deltaX, int deltaY)
 {
+	if(activeLayer == NOACTIVELAYERS){return false;}	//Gestion d'érreur.
+	for(int i;i<couches[activeLayer].VecteurFormesCouche.getTaille();i++)
+   {
+      couches[activeLayer].VecteurFormesCouche.getPointeur(i)->translater(deltaX,deltaY);
+   }
    return true;
 }
 
 void Canevas::afficher(ostream & s)
 {
+	if(activeLayer == NOACTIVELAYERS){}	//Gestion d'erreur.
+	else
+	{
+		for(int y;y<MAX_COUCHES;y++)
+		{
+			cout << "----- Couche " << y << endl;
+			switch(couches[y].etat)
+			{
+				case ETATINITIAL:
+					cout << "Couche initialisee" << endl;
+					break;
+				case ETATCACHE:
+					cout << "Couche cachee" << endl;
+					break;
+				default:
+					for(int i;i<couches[y].VecteurFormesCouche.getTaille();i++)
+					{
+						couches[y].VecteurFormesCouche.getPointeur(i)->afficher(cout);
+					}
+					break;
+			}
+		}
+		//y and i are temporary. But DOOM is ETERNAL!!!
+	}
 }
 
 
